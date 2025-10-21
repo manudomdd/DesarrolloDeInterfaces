faa2f033a5d1afa408a10e9740841a09f351a35e
manudomdd <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-10-21 20:31:56 +0200

    Actualizados

--- EjerciciosTema1/src/Practica1/PantallaPrincipal.java
+++ EjerciciosTema1/src/Practica1/PantallaPrincipal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Practica1;
+package practica1;
 
 import javax.swing.JOptionPane;
 import org.netbeans.validation.api.builtin.stringvalidation.StringValidators;
