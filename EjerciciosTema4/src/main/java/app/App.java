b02e78f7626e600921185bb425073a3b2283e830
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2026-01-19 20:56:38 +0100

    Reporte con filtro

--- EjerciciosTema4/src/main/java/app/App.java
+++ EjerciciosTema4/src/main/java/app/App.java
@@ -4,6 +4,8 @@ import dao.PersonaDAO;
 import repository.ConexionDB;
 import java.sql.Connection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -24,14 +26,22 @@ public class App {
         if (miConexion != null) {
             System.out.println("¡ÉXITO! La conexión se realizó correctamente desde el Main.");   
 
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("------------------------------------------------");
+            System.out.print("Introduce el nombre para filtrar (Enter para ver todos): ");
+            String textoBusqueda = scanner.nextLine();
+
             System.out.println("Generando reporte...");
             
             try {
                 String rutaReporte = "src/main/java/resources/people_report.jrxml.xml";
 
+                Map<String, Object> parametros = new HashMap<>();
+                parametros.put("paramNombre", "%" + textoBusqueda + "%");
+
                 JasperReport reporte = JasperCompileManager.compileReport(rutaReporte);
 
-                JasperPrint print = JasperFillManager.fillReport(reporte, new HashMap<>(), miConexion);
+                JasperPrint print = JasperFillManager.fillReport(reporte, parametros, miConexion);
 
                 JasperViewer.viewReport(print, false); 
                 
@@ -40,6 +50,8 @@ public class App {
             } catch (JRException e) {
                 System.err.println("ERROR AL GENERAR REPORTE:");
                 e.printStackTrace();
+            } finally {
+                scanner.close();
             }
             
         } else {
